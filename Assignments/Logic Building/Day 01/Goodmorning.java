a51c34db563f8d0080636344c912066ed82751f3
Jyotiraditya Patil <jyotiraditya.patil.cmaug26@gmail.com>
2026-08-23 20:53:54 +0530

    Create Goodmorning.java

---
+++ Assignments/Logic Building/Day 01/Goodmorning.java
@@ -0,0 +1,12 @@
+public class Goodmorning {
+    public static void main(String[] args) {
+        int hours = 8;
+
+        if(hours >= 5 && hours <= 12) {
+            System.out.println("Hey Good Morning Dear");
+        }
+        else {
+            System.out.println("Hey its not morning anymore :) ");
+        }
+    }
+}
