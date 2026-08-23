55dc70b109340e47f2edaaff5455321a4df8e2ec
Jyotiraditya Patil <jyotiraditya.patil.cmaug26@gmail.com>
2026-08-23 20:51:19 +0530

    Create Checkoddeven.java

---
+++ Assignments/Logic Building/Day 01/Checkoddeven.java
@@ -0,0 +1,12 @@
+public class Checkoddeven {
+    public static void main(String[] args) {
+        int number = 246;
+
+        if(number % 2 == 0) {
+            System.out.println(number + " Is a is Evem Number ");
+        }
+        else {
+            System.out.println(number + " Is a Odd Number");
+        }
+    }
+}
