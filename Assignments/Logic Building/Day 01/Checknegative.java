bf131d3d76702de6e6cbe104970f4a65b8a293c7
Jyotiraditya Patil <jyotiraditya.patil.cmaug26@gmail.com>
2026-08-23 20:50:01 +0530

    Update Checknegative.java

--- Assignments/Logic Building/Day 01/Checknegative.java
+++ Assignments/Logic Building/Day 01/Checknegative.java
@@ -1,1 +1,12 @@
-
+class Checknegative {
+    public static void main(String[] args) {
+        int number = -4321;
+        
+        if (number <= 0) {
+            System.out.println(number + " Is a Negative Number");
+        }
+        else {
+            System.out.println("The Number is Positive");
+        }
+    }
+}
