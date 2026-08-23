608c8c681342c7e403e0981b46aa65eb83d03da7
Jyotiraditya Patil <jyotiraditya.patil.cmaug26@gmail.com>
2026-08-23 21:23:06 +0530

    Create Uninitializedvalues.java

---
+++ Assignments/Logic Building/Day 02 /Uninitializedvalues.java
@@ -0,0 +1,24 @@
+public class VariableValues {
+    public static void main(String[] args) {
+
+        byte age = 25;
+        short score = 850;
+        int population = 4562;
+        long distance = 18600L;
+
+        float temperature = 28.5f;
+        double percentage = 89.75;
+
+        char grade = 'P';
+        boolean isPassed = true;
+
+        System.out.println("Byte Value       : " + age);
+        System.out.println("Short Value      : " + score);
+        System.out.println("Integer Value    : " + population);
+        System.out.println("Long Value       : " + distance);
+        System.out.println("Float Value      : " + temperature);
+        System.out.println("Double Value     : " + percentage);
+        System.out.println("Character Value  : " + grade);
+        System.out.println("Boolean Value    : " + isPassed);
+    }
+}
