260fc03132404111f2fcbf777d03f0e543677b46
Jyotiraditya Patil <jyotiraditya.patil.cmaug26@gmail.com>
2026-08-23 22:48:40 +0530

    Create Pattern06.java

---
+++ Assignments/Logic Building/Day 04/Pattern06.java
@@ -0,0 +1,14 @@
+public class Pattern06 {
+
+    public static void main(String[] args) {
+
+        for (int row = 1; row <= 5; row++) {
+
+            for (int number = 1; number <= row; number++) {
+                System.out.print(number);
+            }
+
+            System.out.println();
+        }
+    }
+}
