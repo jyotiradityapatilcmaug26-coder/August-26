d166a8f21a06df0af02de663faa103946936d87f
Jyotiraditya Patil <jyotiraditya.patil.cmaug26@gmail.com>
2026-08-23 20:58:54 +0530

    Create Squarearea.java

---
+++ Assignments/Logic Building/Day 01/Squarearea.java
@@ -0,0 +1,10 @@
+public class Squarearea {
+    public static void main(String[] args) {
+
+        double side = 15.75;
+        double squareArea = side * side;
+
+        System.out.println("Side of Square = " + side);
+        System.out.println("Calculated Area = " + squareArea);
+    }
+}
