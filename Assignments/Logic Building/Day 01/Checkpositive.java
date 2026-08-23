c0a091fb4ba5000547be157832d251b0fe279f5f
Jyotiraditya Patil <jyotiraditya.patil.cmaug26@gmail.com>
2026-08-23 20:52:20 +0530

    Create Checkpositive.java

---
+++ Assignments/Logic Building/Day 01/Checkpositive.java
@@ -0,0 +1,12 @@
+class checkPositive {
+    public static void main(String[] args) {
+        int number = 752;
+        
+        if (number >= 0) {
+            System.out.println(number + " Is a Positive Number :)");
+        }
+        else {
+            System.out.println("The Number is Negative");
+        }
+    }
+}
