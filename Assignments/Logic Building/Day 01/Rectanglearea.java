4eb39c3c1406e469de6d3bb7db13eefbc1c138ff
Jyotiraditya Patil <jyotiraditya.patil.cmaug26@gmail.com>
2026-08-23 20:56:24 +0530

    Create Rectanglearea.java

---
+++ Assignments/Logic Building/Day 01/Rectanglearea.java
@@ -0,0 +1,10 @@
+public class Rectanglearea {
+    public static void main(String[] args) {
+
+        double length = 18.50;
+        double breadth = 12.25;
+        double area = length * breadth;
+
+        System.out.println("The area of rectangle is : " + area);
+    }
+}
