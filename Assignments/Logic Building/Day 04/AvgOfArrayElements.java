e01b90b41d744d009bf46bf1144cbac8e1f9b538
Jyotiraditya Patil <jyotiraditya.patil.cmaug26@gmail.com>
2026-08-23 22:35:22 +0530

    Update AvgOfArrayElements.java

--- Assignments/Logic Building/Day 04/AvgOfArrayElements.java
+++ Assignments/Logic Building/Day 04/AvgOfArrayElements.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class AvgOfArrayElements {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[] values = new int[5];
+        int total = 0;
+
+        System.out.println("Enter 5 numbers:");
+
+        for (int index = 0; index < values.length; index++) {
+            values[index] = input.nextInt();
+        }
+
+        for (int value : values) {
+            total += value;
+        }
+
+        double average = total / 5.0;
+
+        System.out.println("Average of the given numbers = " + average);
+
+        input.close();
+    }
+}
