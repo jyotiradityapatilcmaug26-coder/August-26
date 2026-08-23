0dffc7e7364819cdb2f60adbb17aaa4eb073705c
Jyotiraditya Patil <jyotiraditya.patil.cmaug26@gmail.com>
2026-08-23 21:12:25 +0530

    Update CheckLeapYear.java

--- Assignments/Logic Building/Day 02 /CheckLeapYear.java
+++ Assignments/Logic Building/Day 02 /CheckLeapYear.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
 
+class CheckLeapYear {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the year: ");
+        int enteredYear = input.nextInt();
+
+        boolean isLeap = false;
+
+        if (enteredYear % 400 == 0) {
+            isLeap = true;
+        }
+        else if (enteredYear % 100 == 0) {
+            isLeap = false;
+        }
+        else if (enteredYear % 4 == 0) {
+            isLeap = true;
+        }
+
+        if (isLeap) {
+            System.out.println(enteredYear + " is a Leap Year.");
+        }
+        else {
+            System.out.println(enteredYear + " is not a Leap Year.");
+        }
+
+        input.close();
+    }
+}
